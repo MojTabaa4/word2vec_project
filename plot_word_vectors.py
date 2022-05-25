dd5a5305d4294f6c2090c2b9b69f10d50ab33afd
Mojtaba Abtahi <mojtaba.abtahi@zoodfood.com>
2022-05-25 23:20:11 +0430

    add choose between 2d and 3d plot define with args

--- plot_word_vectors.py
+++ plot_word_vectors.py
@@ -1,14 +1,18 @@
+import sys
+
 import pandas as pd
 import plotly.express as px
 
 if __name__ == '__main__':
-    dims2d = pd.read_pickle("dim2d.pkl")
-    dims3d = pd.read_pickle("dim3d.pkl")
-
-    # For 2d interactive scatter plot
-    fig = px.scatter(dims2d, x="x", y="y", symbol="token")
-
-    # For 3d interactive scatter plot
-    # fig = px.scatter_3d(dims3d, x='x', y='y', z='z',  symbol='token')
-
-    fig.show()
+    if sys.argv[1] == '2d':
+        # For 2d interactive scatter plot
+        dims2d = pd.read_pickle("results/dim2d.pkl")
+        fig = px.scatter(dims2d, x="x", y="y", symbol="token")
+        fig.show()
+    elif sys.argv[1] == '3d':
+        # For 3d interactive scatter plot
+        dims3d = pd.read_pickle("results/dim3d.pkl")
+        fig = px.scatter_3d(dims3d, x='x', y='y', z='z', color='token')
+        fig.show()
+    else:
+        print('Please choose between 2d and 3d args')
